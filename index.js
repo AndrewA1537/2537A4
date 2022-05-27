4a152aa1f91d4a43f14fe7f63e09cc8db05ed627
Andrew Anca <andrewanca1998@hotmail.com>
2022-05-27 10:02:13 -0700

    basic game functionality working and implemented tutorial

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-cardHasBeenFlipped = false
+// var scope   = global OR function/local
+// let scope   = block
+// const scope = block
+const cards = document.querySelectorAll(".card");
 
-secondCard = undefined
-firstCard = undefined
+cardHasBeenFlipped = false;
+
+firstCard = undefined;
+secondCard = undefined;
+
+
+let lockBoard = false;
 
 function checkForMatch()
 {
@@ -23,35 +31,56 @@ function disableCards()
     // if match
     $(`#${firstCard.id}`).parent().off("click");
     $(`#${secondCard.id}`).parent().off("click");
-}
+};
 
 function unflipCards()
 {
+    lockBoard = true;
+
     // if not a match
     console.log("not a Match!");
     setTimeout(() =>
     {
         $(`#${firstCard.id}`).parent().removeClass("flip");
         $(`#${secondCard.id}`).parent().removeClass("flip");
+
+        lockBoard = false;
     }, 1000);
-}
+;}
+
+function resetBoard()
+{
+    [cardHasBeenFlipped, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+};
+
+(function shuffleCards() 
+{
+    cards.forEach(card => {
+        let randomInt = Math.floor(Math.random() * 6);
+        card.style.order = randomInt;
+    });
+})();
 
 function setup()
 {
     $(".card").on("click", function()
     {
-        $(this).toggleClass("flip")
+        $(this).toggleClass("flip");
+
+        if (lockBoard) return;
+        if (this == firstCard) return;
 
         if (!cardHasBeenFlipped)
         {
             //first click
-            firstCard = $(this).find(".front_face")[0]
-            cardHasBeenFlipped = true
+            firstCard = $(this).find(".front_face")[0];
+            cardHasBeenFlipped = true;
         }
         else
         {
             // second click
-            secondCard = $(this).find(".front_face")[0]
+            secondCard = $(this).find(".front_face")[0];
             cardHasBeenFlipped = false;
             checkForMatch();
         };
