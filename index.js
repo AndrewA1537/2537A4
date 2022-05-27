71d39e28989e2176c7f756f115640e34532d7999
Andrew Anca <andrewanca1998@hotmail.com>
2022-05-27 09:38:28 -0700

    update to styling and separating functions for more readability

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
+cardHasBeenFlipped = false
+
 secondCard = undefined
 firstCard = undefined
 
-cardHasBeenFlipped = false
+function checkForMatch()
+{
+    // if match
+    if ($(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src"))
+    {
+        console.log("A Match!");
+        disableCards();
+    }
+    else
+    {
+        // if not a match
+        unflipCards();
+    };
+};
 
-function setup() {
-    $(".card").on("click", function() {
-        $(this).toggleClass("flip")
-
-        if (!cardHasBeenFlipped) {
-            //captured first card
-            firstCard = $(this).find(".front_face")[0]
-                // console.log(firstCard);
-            cardHasBeenFlipped = true
-        } else {
-            secondCard = $(this).find(".front_face")[0]
-            console.log(firstCard, secondCard);
-            cardHasBeenFlipped = false;
-
-            // check if you have match
-            if (
-                $(`#${firstCard.id}`).attr("src") ==
-                $(`#${secondCard.id}`).attr("src")
-            ) {
-                console.log("A Match!");
-                $(`#${firstCard.id}`).parent().off("click")
-                $(`#${secondCard.id}`).parent().off("click")
-            } else {
-                console.log("not a Match!");
-                setTimeout(() => {
-                    $(`#${firstCard.id}`).parent().removeClass("flip")
-                    $(`#${secondCard.id}`).parent().removeClass("flip")
-                }, 1000)
-
-            }
-        }
-
-
-
-        // reset 
-        // firstCard = undefined
-        // secondCard = undefined
-
-    })
+function disableCards()
+{
+    // if match
+    $(`#${firstCard.id}`).parent().off("click");
+    $(`#${secondCard.id}`).parent().off("click");
 }
 
+function unflipCards()
+{
+    // if not a match
+    console.log("not a Match!");
+    setTimeout(() =>
+    {
+        $(`#${firstCard.id}`).parent().removeClass("flip");
+        $(`#${secondCard.id}`).parent().removeClass("flip");
+    }, 1000);
+}
 
+function setup()
+{
+    $(".card").on("click", function()
+    {
+        $(this).toggleClass("flip")
 
-$(document).ready(setup)
+        if (!cardHasBeenFlipped)
+        {
+            //first click
+            firstCard = $(this).find(".front_face")[0]
+            cardHasBeenFlipped = true
+        }
+        else
+        {
+            // second click
+            secondCard = $(this).find(".front_face")[0]
+            cardHasBeenFlipped = false;
+            checkForMatch();
+        };
+    });
+};
+
+$(document).ready(setup);
